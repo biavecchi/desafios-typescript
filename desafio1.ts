12a660a838ebc1b439b02c5fa22aa88c96639216
Bianca V Martins <85034782+biavecchi@users.noreply.github.com>
2022-07-02 15:04:58 -0300

    Add files via upload

---
+++ desafio1.ts
@@ -0,0 +1,19 @@
+let employee = {
+    code: 10,
+    name: "John"
+};
+
+let employee2: {code: number, name: string} = {
+    code: 10,
+    name: "John"
+}
+
+interface employee {
+    code: 10,
+    name: string
+}
+
+let employee3: employee = {
+    code: 10, 
+    name: "John"
+}
